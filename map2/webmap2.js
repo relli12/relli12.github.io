79af2ef041d952d93d4d3848693e34787eee6a0c
Rachel C Ellis <relli13@lsu.edu>
2019-03-26 11:49:38 -0500

    map 2

---
+++ map2/webmap2.js
@@ -0,0 +1,8 @@
+let myMap2 = L.map('map2').setView([32.18, -99.14], 4)
+L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_nolabels/{z}/{x}/{y}.png').addTo(myMap2)
+L.tileLayer.wms('http://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi', {
+  layers: 'nexrad-n0r-900913',
+  format: 'image/png',
+  transparent: true,
+  attribution: 'NOAA, Iowa State University'
+}).addTo(myMap2)
