00ec88bdd11f936e5fcf404467c9c0adaf1d10b1
Rachel C Ellis <relli13@lsu.edu>
2019-04-04 11:53:00 -0500

    updated

--- map3/webmap3.js
+++ map3/webmap3.js
@@ -1,11 +1,25 @@
-let myMap3 = L.map('map3').setView([32.18, -99.14], 4)
-L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager_labels_under/{z}/{x}/{y}{r}.png').addTo(myMap3)
-L.tileLayer.wms('https://nowcoast.noaa.gov/arcgis/services/nowcoast/forecast_meteoceanhydro_sfc_ndfd_qpf6hrs_offsets/MapServer/WMSServer?request=GetCapabilities&service=WMS&version=1.3.0', {
-  layers: '1',
-  format: 'image/png',
-  transparent: true,
-  attribution: 'NOAA'
-}).addTo(myMap3)
-L.esri.dynamicMapLayer({
-  url: 'http://hydro.arcgis.com/arcgis/rest/services/Tools/Hydrology/GPServer/Watershed/submitJob'
-}).addTo(myMap3)
+let stateMap = L.map('map3').setView([32.18, -99.14], 4)
+L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager_labels_under/{z}/{x}/{y}{r}.png').addTo(stateMap)
+let stateDemographicsUrl = 'https://geog4046.github.io/portfolio/data/us_state_demographics_ESRI_2010A.geojson'
+jQuery.getJSON(stateDemographicsUrl, function (data) {
+  let stateStyle = function (feature) {
+  let age = feature.properties.MED_AGE // get the current state's Median Age attribute
+  let stateColor = 'olive' // let the initial color be a darker green
+  if ( age < 38 ) { stateColor = 'green' } // if the state's median age is less than the average, color it a lighter green
+  return {
+    color: stateColor, //use the color variable above for the value
+    weight: 1,
+    fillOpacity: 0.2
+  }
+}
+let onEachFeature = function (feature, layer) {
+  let name = feature.properties.STATE_NAME
+    let age = feature.properties.MED_AGE
+    layer.bindPopup('Median age of ' + name + ': ' + age + '<br>National average: 38')    // contents of the function
+   }
+  let geojsonOptions= {
+    style:stateStyle,
+    onEachFeature: onEachFeature
+  }
+  L.geoJSON(data, geojsonOptions).addTo(stateMap)
+})
