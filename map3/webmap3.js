a3e2b8dcd14350c87106b1cdfd0c76360bfb5f86
relli12 <48488110+relli12@users.noreply.github.com>
2019-04-27 17:41:19 -0500

    Update webmap3.js

--- map3/webmap3.js
+++ map3/webmap3.js
@@ -1,25 +1,29 @@
-let stateMap = L.map('map3').setView([32.18, -99.14], 4)
-L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager_labels_under/{z}/{x}/{y}{r}.png').addTo(stateMap)
+let stateMapByGender = L.map('map3').setView([32.18, -99.14], 4)
+L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager_labels_under/{z}/{x}/{y}{r}.png').addTo(stateMapByGender)
 let stateDemographicsUrl = 'https://geog4046.github.io/portfolio/data/us_state_demographics_ESRI_2010A.geojson'
 jQuery.getJSON(stateDemographicsUrl, function (data) {
   let stateStyle = function (feature) {
-  let age = feature.properties.MED_AGE // get the current state's Median Age attribute
-  let stateColor = 'olive' // let the initial color be a darker green
-  if ( age < 38 ) { stateColor = 'green' } // if the state's median age is less than the average, color it a lighter green
+  let numberOfFemales = feature.properties.FEMALES //
+  let stateColor = 'pink' //
+  let numberOfMales= feature.properties.MALES
+  if ( numberOfFemales < numberOfMales ) { stateColor = 'blue' } //
   return {
-    color: stateColor, //use the color variable above for the value
+    color: stateColor, //
     weight: 1,
     fillOpacity: 0.2
   }
 }
 let onEachFeature = function (feature, layer) {
   let name = feature.properties.STATE_NAME
-    let age = feature.properties.MED_AGE
-    layer.bindPopup('Median age of ' + name + ': ' + age + '<br>National average: 38')    // contents of the function
+    let numberOfFemales = feature.properties.FEMALES
+    let numberOfMales= feature.properties.MALES
+    layer.bindPopup('Number of Females ' + name + ': ' + numberOfFemales + '2010 Census average: 3,132,934' )
    }
+   layer.bindPopup('Number of Males ' + name + ': ' + numberOfMales + '2010 Census average: 3,029,942.08' )
+ }
   let geojsonOptions= {
     style:stateStyle,
     onEachFeature: onEachFeature
   }
-  L.geoJSON(data, geojsonOptions).addTo(stateMap)
+  L.geoJSON(data, geojsonOptions).addTo(stateMapByGender)
 })
