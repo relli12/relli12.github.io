f6a3be051ff4944cd468d28ee076aff9cbb360e0
relli12 <48488110+relli12@users.noreply.github.com>
2019-05-02 16:59:34 -0500

    Update webmapFinal.js

--- finalMap/webmapFinal.js
+++ finalMap/webmapFinal.js
@@ -1,5 +1,6 @@
-let stateMap = L.map('map3').setView([32.18, -99.14], 4)
-L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager_labels_under/{z}/{x}/{y}{r}.png').addTo(stateMap)
+let trafficMap = L.map('map3').setView([32.18, -99.14], 4)
+L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager_labels_under/{z}/{x}/{y}{r}.png').addTo(trafficMap)
+L.tileLayer('https://services9.arcgis.com/SDQDNhpG8jikA0D1/arcgis/rest/services/Baton_Rouge_Traffic_Incidents_2010_to_2019_RE/FeatureServer').addTo(trafficMap)
 let stateDemographicsUrl = 'https://geog4046.github.io/portfolio/data/us_state_demographics_ESRI_2010A.geojson'
 jQuery.getJSON(stateDemographicsUrl, function (data) {
   let stateStyle = function (feature) {
@@ -21,5 +22,5 @@ let onEachFeature = function (feature, layer) {
     style:stateStyle,
     onEachFeature: onEachFeature
   }
-  L.geoJSON(data, geojsonOptions).addTo(stateMap)
+  L.geoJSON(data, geojsonOptions).addTo(trafficMap)
 })
