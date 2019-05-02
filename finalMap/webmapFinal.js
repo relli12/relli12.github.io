08acf015df195f0e43511aa8276caca0ee99a1d4
relli12 <48488110+relli12@users.noreply.github.com>
2019-05-02 18:15:17 -0500

    Update webmapFinal.js

--- finalMap/webmapFinal.js
+++ finalMap/webmapFinal.js
@@ -1,11 +1,9 @@
-let trafficMap = L.map('map4').setView([32.18, -99.14], 4)
+let trafficMap = L.map('map4').setView([30.45, -91.19], 4)
 L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager_labels_under/{z}/{x}/{y}{r}.png').addTo(trafficMap)
-L.tileLayer('https://services9.arcgis.com/SDQDNhpG8jikA0D1/arcgis/rest/services/Baton_Rouge_Traffic_Incidents_2010_to_2019_RE/FeatureServer').addTo(trafficMap)
-let stateDemographicsUrl = 'https://geog4046.github.io/portfolio/data/us_state_demographics_ESRI_2010A.geojson'
+let batonRougeTrafficIncidentsUrl = 'https://services9.arcgis.com/SDQDNhpG8jikA0D1/arcgis/rest/services/Baton_Rouge_Traffic_Incidents_2010_to_2019_RE/FeatureServer'
 jQuery.getJSON(stateDemographicsUrl, function (data) {
-let stateMapByGender = L.map('map3').setView([32.18, -99.14], 4)
+let trafficMap= L.map('map4').setView([30.45, -91.19], 4)
 L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager_labels_under/{z}/{x}/{y}{r}.png').addTo(trafficMap)
-let stateDemographicsUrl = 'https://geog4046.github.io/portfolio/data/us_state_demographics_ESRI_2010A.geojson'
 jQuery.getJSON(stateDemographicsUrl, function (data) {
   let stateStyle = function (feature) {
   let batonRougeTrafficIncidents = feature.properties.Baton_Rouge_Traffic_Incidents //
@@ -18,13 +16,13 @@ jQuery.getJSON(stateDemographicsUrl, function (data) {
   }
 }
 let onEachFeature = function (feature, layer) {
-  let name = feature.properties.STATE_NAME
-    let numberOfFemales = feature.properties.Baton_Rouge_Traffic_Incidents
-    layer.bindPopup('Number of Females' + 'Census average: 3132934' )
+  let name = feature.properties.Baton_Rouge_Traffic_Incidents
+    let batonRougeTrafficIncidents = feature.properties.Baton_Rouge_Traffic_Incidents
+    layer.bindPopup('FILE#' + 'CRASH DATE'+'TOT VEH'+'DISTRICT'+'ZONE'+'SUBZONE'+'STREET#'+'STREET NAME'+'STREET NAME'+'FORMATTED STREET' )
  }
   let geojsonOptions= {
     style:stateStyle,
     onEachFeature: onEachFeature
   }
-  L.geoJSON(data, geojsonOptions).addTo(stateMapByGender)
+  L.geoJSON(data, geojsonOptions).addTo(trafficMap)
 })
