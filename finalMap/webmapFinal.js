cfcf4ecdcf5434a6db3732bbd83af0df5d27df6e
relli12 <48488110+relli12@users.noreply.github.com>
2019-05-03 15:00:56 -0500

    Update webmapFinal.js

--- finalMap/webmapFinal.js
+++ finalMap/webmapFinal.js
@@ -2,7 +2,7 @@ let trafficMap = L.map('map4').setView([30.4515, -91.1871], 3)
 L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager_labels_under/{z}/{x}/{y}{r}.png').addTo(trafficMap)
 let batonRougeTrafficIncidentsUrl = 'https://arcg.is/0rrLj9'
 // jQuery.getJSON(batonRougeTrafficIncidentsUrl, function (data) {
-// let trafficMap= L.map('map4').setView([30.4515, -91.1871], 4)
+// let trafficMap= L.map('map4').setView([30.4515, -91.1871], 3)
 // L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager_labels_under/{z}/{x}/{y}{r}.png').addTo(trafficMap)
 jQuery.getJSON(batonRougeTrafficIncidentsUrl, function (data) {
   let incidentStyle = function (feature) {
