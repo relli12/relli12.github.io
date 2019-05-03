19014b3f3293af70c2de74310a070a3fb0e1795b
relli12 <48488110+relli12@users.noreply.github.com>
2019-05-03 15:54:40 -0500

    Update webmapFinal.js

--- finalMap/webmapFinal.js
+++ finalMap/webmapFinal.js
@@ -6,9 +6,9 @@ let batonRougeTrafficIncidentsUrl = 'https://data.brla.gov/resource/epwd-efpa.ge
 // L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager_labels_under/{z}/{x}/{y}{r}.png').addTo(trafficMap)
 jQuery.getJSON(batonRougeTrafficIncidentsUrl, function (data) {
   let incidentStyle = function (feature) {
-    let batonRougeTrafficIncidents = feature.properties.TOT_VEH //
+    let batonRougeTrafficIncidents = feature.properties.LIGHTING //
     let incidentColor = 'pink' //
-    if ( TOT_VEH > 4) { incidentColor = 'blue' } //
+    if (LIGHTING= DAYLIGHT) { incidentColor = 'blue' } //
     return {
       color: incidentColor, //
       weight: 1,
