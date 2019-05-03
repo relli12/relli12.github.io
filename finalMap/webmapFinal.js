6552b9007c7c93b8fcf6aed387e11074f6062970
relli12 <48488110+relli12@users.noreply.github.com>
2019-05-03 00:54:21 -0500

    Update webmapFinal.js

--- finalMap/webmapFinal.js
+++ finalMap/webmapFinal.js
@@ -1,39 +1,38 @@
 let trafficMap = L.map('map4').setView([30.4515, -91.1871], 4)
 L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager_labels_under/{z}/{x}/{y}{r}.png').addTo(trafficMap)
 let batonRougeTrafficIncidentsUrl = 'https://services9.arcgis.com/SDQDNhpG8jikA0D1/arcgis/rest/services/Baton_Rouge_Traffic_Incidents_2010_to_2019_RE/FeatureServer'
-jQuery.getJSON(batonRougeTrafficIncidentsUrl, function (data) {
-let trafficMap= L.map('map4').setView([30.4515, -91.1871], 4)
-L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager_labels_under/{z}/{x}/{y}{r}.png').addTo(trafficMap)
+// jQuery.getJSON(batonRougeTrafficIncidentsUrl, function (data) {
+// let trafficMap= L.map('map4').setView([30.4515, -91.1871], 4)
+// L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager_labels_under/{z}/{x}/{y}{r}.png').addTo(trafficMap)
 jQuery.getJSON(batonRougeTrafficIncidentsUrl, function (data) {
   let incidentStyle = function (feature) {
-  let batonRougeTrafficIncidents = feature.properties.Baton_Rouge_Traffic_Incidents //
-  let incidentColor = 'pink' //
-  if ( batonRougeTrafficIncidents > 4) { incidentColor = 'blue' } //
-  return {
-    color: incidentColor, //
-    weight: 1,
-    fillOpacity: 0.2
+    let batonRougeTrafficIncidents = feature.properties.Baton_Rouge_Traffic_Incidents //
+    let incidentColor = 'pink' //
+    if ( batonRougeTrafficIncidents > 4) { incidentColor = 'blue' } //
+    return {
+      color: incidentColor, //
+      weight: 1,
+      fillOpacity: 0.2
+    }
   }
-}
-let onEachFeature = function (feature, layer) {
-  let name = feature.properties.Baton_Rouge_Traffic_Incidents
+  let onEachFeature = function (feature, layer) {
+    let name = feature.properties.Baton_Rouge_Traffic_Incidents
     let batonRougeTrafficIncidents = feature.properties.Baton_Rouge_Traffic_Incidents
     layer.bindPopup('FILE#' + 'CRASH DATE'+'TOT VEH'+'DISTRICT'+'ZONE'+'SUBZONE'+'STREET#'+'STREET NAME'+'STREET NAME'+'FORMATTED STREET' )
- }
- let trafficIncidentsNearSouthernCampus=L.polygon([
-   [30.543096,-91.20610],
-   [30.526674,-91.102056],
-   [30.501922,-91.149096]
- ]).addTo(trafficMap)
- let trafficIncidentsNearLSUCampus=L.polygon([
-   [30.411965,-91.194843],
-   [30.433186,-91.157895],
-   [30.386532,-91.159627]
- ]).addTo(trafficMap)
-  let geojsonOptions= {
-    style:stateStyle,
+  }
+  let trafficIncidentsNearSouthernCampus=L.polygon([
+    [30.543096,-91.20610],
+    [30.526674,-91.102056],
+    [30.501922,-91.149096]
+  ]).addTo(trafficMap)
+  let trafficIncidentsNearLSUCampus=L.polygon([
+    [30.411965,-91.194843],
+    [30.433186,-91.157895],
+    [30.386532,-91.159627]
+  ]).addTo(trafficMap)
+  let geojsonOptions = {
+    style: incidentStyle,
     onEachFeature: onEachFeature
   }
   L.geoJSON(data, geojsonOptions).addTo(trafficMap)
-})
 })
