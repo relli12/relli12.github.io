2c9f5d9448c1f5c63509f5d6023a654270b27a72
relli12 <48488110+relli12@users.noreply.github.com>
2019-05-03 14:50:12 -0500

    Update webmapFinal.js

--- finalMap/webmapFinal.js
+++ finalMap/webmapFinal.js
@@ -1,4 +1,4 @@
-let trafficMap = L.map('map4').setView([30.4515, -91.1871], 3)
+let trafficMap = L.map('map4').setView([30.4515, -91.1871], 4)
 L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager_labels_under/{z}/{x}/{y}{r}.png').addTo(trafficMap)
 let batonRougeTrafficIncidentsUrl = 'https://data.brla.gov/resource/epwd-efpa.json'
 // jQuery.getJSON(batonRougeTrafficIncidentsUrl, function (data) {
@@ -6,7 +6,7 @@ let batonRougeTrafficIncidentsUrl = 'https://data.brla.gov/resource/epwd-efpa.js
 // L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager_labels_under/{z}/{x}/{y}{r}.png').addTo(trafficMap)
 jQuery.getJSON(batonRougeTrafficIncidentsUrl, function (data) {
   let incidentStyle = function (feature) {
-    let batonRougeTrafficIncidents = feature.properties. TOT_VEH //
+    let batonRougeTrafficIncidents = feature.properties.TOT_VEH //
     let incidentColor = 'pink' //
     if ( TOT_VEH > 4) { incidentColor = 'blue' } //
     return {
@@ -16,7 +16,7 @@ jQuery.getJSON(batonRougeTrafficIncidentsUrl, function (data) {
     }
   }
   let onEachFeature = function (feature, layer) {
-    let name = feature.properties.Baton_Rouge_Traffic_Incidents
+    let name = feature.properties.FILE#
     let batonRougeTrafficIncidents = feature.properties.Baton_Rouge_Traffic_Incidents
     layer.bindPopup('FILE#'+'CRASH DATE'+'TOT VEH'+'DISTRICT'+'ZONE'+'SUBZONE'+'STREET#'+'STREET NAME'+'STREET NAME'+'FORMATTED STREET' )
   }
@@ -36,4 +36,3 @@ jQuery.getJSON(batonRougeTrafficIncidentsUrl, function (data) {
   }
   L.geoJSON(data, geojsonOptions).addTo(trafficMap)
 })
-
